0fc8bcb0ab9084f40254e601dccf89c1e6a96dee
PLyn <plyn8750@gmail.com>
2025-07-19 19:01:54 -0500

    small updates

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,20 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+		kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/supabase-storage-migrator' : ''
+		}
+	}
 };
 
 export default config;
